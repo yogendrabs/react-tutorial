b11565b67fc4caede6ef5e04f9e0ab6af58fa6ce
YBS <id_ybs@hotmail.com>
2019-12-13 12:08:33 +1100

    1st commit since start. reached to props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    render() {
+
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Actress',
+            },{
+                name: 'Dennis',
+                job: 'Bartender',
+            }
+        ];
+
+        return (
+            <div className="container">
+                {/* <h1>Hello, React!</h1> */}
+                <Table characterData = {characters}/>
+            </div>
+        )
+    }
+};
 
 export default App;
