72039f193c6cde64b72c4b37b98bb5da7dd1b609
YBS <id_ybs@hotmail.com>
2019-12-13 15:09:56 +1100

    Updates

---
+++ precache-manifest.c01c9cc803b20f44dfa6e9499d58b77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b987cd0c65658ce685f151ab16f8da73",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "697800e7e9ca8b261f49",
+    "url": "/react-tutorial/static/css/main.085aecb3.chunk.css"
+  },
+  {
+    "revision": "1ab8a11d26d36a982dbe",
+    "url": "/react-tutorial/static/js/2.ea385857.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.ea385857.chunk.js.LICENSE"
+  },
+  {
+    "revision": "697800e7e9ca8b261f49",
+    "url": "/react-tutorial/static/js/main.afeda0a4.chunk.js"
+  },
+  {
+    "revision": "b9cf8d7406673e674e0b",
+    "url": "/react-tutorial/static/js/runtime-main.af30ece7.js"
+  }
+]);
